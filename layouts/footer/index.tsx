a3c07163365147e5c95c4a67ff25ae854dc99eb5
jacobhsu <jacob.hsu.tw@gmail.com>
2022-12-07 11:34:20 +0800

    @layout/footer

---
+++ layouts/footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => (
+    <footer className="flex items-center justify-center w-full h-24 border-t">
+        <a
+            className="flex items-center justify-center gap-2"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            Powered by{" "}Next
+        </a>
+    </footer>
+);
+
+export default Footer;
