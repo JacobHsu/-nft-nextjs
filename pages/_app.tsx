067298b7b1240b9c562d31ff0f05abcd34676aa9
jacobhsu <jacob.hsu.tw@gmail.com>
2022-12-08 14:32:09 +0800

    fix: _app next-themes ThemeProvider ColorSwitcher next-themes useTheme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
 import "../assets/css/bootstrap.min.css";
 import "../assets/scss/style.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeProvider defaultTheme="dark">
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
